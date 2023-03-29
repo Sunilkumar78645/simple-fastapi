5a2b1d274ac9168462674d8c78e1772b3b0dde65
learner-404 <129073405+learner-404@users.noreply.github.com>
2023-03-29 12:04:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 import uvicorn
 from fastapi import FastAPI
-from promptgens import promptgen
-# import nltkmodules
-import numpy as np
-import pandas as pd
-# import pickle
-from writers import writer
 
 app = FastAPI()
 
@@ -18,16 +12,9 @@ def get_name(name: str):
     return {'Welcome ': f'{name}'}
 
 
-@app.post('/prompt')
-# writer(sentence)
-def promptgenn(data:promptgen):
-    data = data.dict()
-    sentence = data['sentence']
-    # print(writer(sentence))
-    prediction = writer(sentence)
-    return {
-        'prediction': prediction
-    }
+@app.post('/trial')
+def get_name(name: str):
+    return {'Welcome ': f'{name}'}
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
