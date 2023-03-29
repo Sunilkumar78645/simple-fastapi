e5cb1e82b5b8945452ff17021d82023f7680c4c7
learner-404 <129073405+learner-404@users.noreply.github.com>
2023-03-29 12:01:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+import uvicorn
+from fastapi import FastAPI
+from promptgens import promptgen
+# import nltkmodules
+import numpy as np
+import pandas as pd
+# import pickle
+from writers import writer
+
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return {'message': 'Hello, World'}
+
+@app.get('/{name}')
+def get_name(name: str):
+    return {'Welcome ': f'{name}'}
+
+
+@app.post('/prompt')
+# writer(sentence)
+def promptgenn(data:promptgen):
+    data = data.dict()
+    sentence = data['sentence']
+    # print(writer(sentence))
+    prediction = writer(sentence)
+    return {
+        'prediction': prediction
+    }
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
+    
+#uvicorn app:app --reload
