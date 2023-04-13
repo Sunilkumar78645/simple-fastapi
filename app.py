62d0dc82d3714e8536980a08f835966f3d5df09c
Sunilkumar78645 <95273883+Sunilkumar78645@users.noreply.github.com>
2023-04-13 17:06:48 +0530

    AEEEEEEEEEEEEEEEEEEEEEE

--- app.py
+++ app.py
@@ -17,6 +17,6 @@ def get_name(name: str):
     return {'Welcome ': f'{name}'}
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    app.run()
     
 # uvicorn app:app --reload
